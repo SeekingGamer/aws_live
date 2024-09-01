28c3bfb5993d8e3b86166c30e673d1662166d2f8
SeekingGamer <160568653+SeekingGamer@users.noreply.github.com>
2024-09-01 22:16:12 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cjhrv1g4cndb.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "aws_user"
+customdb = "employee"
+custombucket = "ngthengfung-employee"
+customregion = "us-east-1"
+
